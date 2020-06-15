8d8f9b26880658b2de5563166f59cfcdf73bb466
luiing <luiing@foxmail.com>
2020-06-15 23:42:44 +0800

    拓展方法

--- groupadapter/src/main/java/com/uis/groupadapter/GroupHolder.kt
+++ groupadapter/src/main/java/com/uis/groupadapter/GroupHolder.kt
@@ -8,5 +8,5 @@ import androidx.recyclerview.widget.RecyclerView
 abstract class GroupHolder<T : Any> : RecyclerView.ViewHolder{
     constructor(view: View):super(view)
     constructor(layoutRes :Int,parent :ViewGroup):super(LayoutInflater.from(parent.context).inflate(layoutRes,parent,false))
-    abstract fun bindVH(item: T)
+    fun bindVH(item: T){}
 }
